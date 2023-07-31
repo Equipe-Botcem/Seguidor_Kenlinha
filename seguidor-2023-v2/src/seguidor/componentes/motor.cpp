54cb5032483a10b976925787bd4043018614e4dc
André Luís Corrêa Barbado <104315238+andrebarbadoUFSC@users.noreply.github.com>
2023-07-31 19:39:38 -0300

    Update motor.cpp

--- seguidor-2023-v2/src/seguidor/componentes/motor.cpp
+++ seguidor-2023-v2/src/seguidor/componentes/motor.cpp
@@ -1,5 +1,6 @@
 #include "motor.hpp"
 
+// Define a direção do motor com base no parâmetro fornecido
 void motor::set_direcao(char dir){
     switch (dir)
     {
@@ -7,7 +8,7 @@ void motor::set_direcao(char dir){
         digitalWrite(pin_DIR1, sinal);
 	    digitalWrite(pin_DIR2, !sinal);
         break;
-    case 'T': //Tras
+    case 'T': //Trás
         digitalWrite(pin_DIR1, !sinal);
 	    digitalWrite(pin_DIR2, sinal);
         break;
@@ -24,16 +25,22 @@ void motor::set_direcao(char dir){
     }
     direcao = dir;
 }
+
+// Construtor: inicializa os valores dos pinos do motor e define a direção para 'Frente'
 motor::motor()
 {
     pin_PWM = 0; pin_DIR1 =-1; pin_DIR2 = -1;
     sinal = HIGH; set_direcao('F');
 }
+
+// Configura os pinos do motor
 void motor::set_pins(int p_pwm, int p_dir1, int p_dir2)
 {
     pin_PWM = p_pwm; pin_DIR1 = p_dir1; pin_DIR2 = p_dir2;
     sinal = HIGH; set_direcao('F');
 }
+
+// Define a velocidade do motor, ajustando a direção se necessário
 void motor::set_velocidade(int vel){
     if(vel < -255) vel = -255;
     else if(vel > 255) vel = 255;
@@ -44,6 +51,7 @@ void motor::set_velocidade(int vel){
     velocidade = vel;
 }
 
+// Similar à função set_velocidade, mas não realiza nenhuma verificação de limite de velocidade
 void motor::set_velocidade_fast(int vel){
     sinal = vel >= 0 ? HIGH : LOW;
     set_direcao(direcao);
@@ -51,10 +59,12 @@ void motor::set_velocidade_fast(int vel){
     velocidade = vel;
 }
 
+// Retorna a direção atual do motor
 char motor::get_direcao(){
     return direcao;
 }
+
+// Retorna a velocidade atual do motor
 int motor::get_velocidade(){
     return velocidade;
 }
-
