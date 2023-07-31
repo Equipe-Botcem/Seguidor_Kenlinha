e4a9a929d1dca4de09c25eda4c48ab5c5753c132
André Luís Corrêa Barbado <104315238+andrebarbadoUFSC@users.noreply.github.com>
2023-07-31 19:47:23 -0300

    Update motor.hpp

--- seguidor-2023-v2/lib/SDL-LIB/motor.hpp
+++ seguidor-2023-v2/lib/SDL-LIB/motor.hpp
@@ -1,23 +1,34 @@
+// Inclui a biblioteca do Arduino.
 #include "Arduino.h"
+
+// Garante que a biblioteca não seja incluída mais de uma vez.
 #ifndef MOTOR_H
 #define MOTOR_H
+
+// Define a classe motor.
 class motor
 {
 private:
-    char direcao;
-    int sinal; //pode ser invertido por velocidade negativa
-    int pin_PWM, pin_DIR1, pin_DIR2;
-    int velocidade;
+    // Variáveis privadas.
+    char direcao; // Indica a direção atual do motor.
+    int sinal; // Indica se a velocidade é positiva ou negativa.
+    int pin_PWM, pin_DIR1, pin_DIR2; // Pinagem para a ligação com o Arduino.
+    int velocidade; // Armazena a velocidade atual do motor.
+
 public:
+    // Construtor.
     motor();
     
-    void set_pins(int p_pwm, int p_dir1, int p_dir2);
-    void set_direcao(char dir);
-    void set_velocidade(int vel);
-    void set_velocidade_fast(int vel);
+    // Funções de configuração.
+    void set_pins(int p_pwm, int p_dir1, int p_dir2); // Define a pinagem.
+    void set_direcao(char dir); // Define a direção do motor.
+    void set_velocidade(int vel); // Define a velocidade do motor.
+    void set_velocidade_fast(int vel); // Define rapidamente a velocidade do motor.
 
-    char get_direcao();
-    int get_velocidade();
-    
+    // Funções de consulta.
+    char get_direcao(); // Retorna a direção atual do motor.
+    int get_velocidade(); // Retorna a velocidade atual do motor.
 };
+
+// Fecha a definição de prevenção de dupla inclusão.
 #endif
