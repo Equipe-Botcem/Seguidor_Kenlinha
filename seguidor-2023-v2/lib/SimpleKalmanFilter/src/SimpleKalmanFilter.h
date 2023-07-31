901e259bca14218259b7c516ed2aaf5b1c73ea87
André Luís Corrêa Barbado <104315238+andrebarbadoUFSC@users.noreply.github.com>
2023-07-31 19:53:23 -0300

    Update SimpleKalmanFilter.h

--- seguidor-2023-v2/lib/SimpleKalmanFilter/src/SimpleKalmanFilter.h
+++ seguidor-2023-v2/lib/SimpleKalmanFilter/src/SimpleKalmanFilter.h
@@ -4,29 +4,39 @@
  * Released under MIT License - see LICENSE file for details.
  */
 
+// Proteção contra inclusão múltipla. Se a biblioteca já foi incluída, não a inclui novamente.
 #ifndef SimpleKalmanFilter_h
 #define SimpleKalmanFilter_h
 
+// Definição da classe SimpleKalmanFilter.
 class SimpleKalmanFilter
 {
-
 public:
+  // Construtor que recebe três parâmetros: erro de medição, erro de estimativa e ruído do processo.
   SimpleKalmanFilter(float mea_e, float est_e, float q);
+  
+  // Método para atualizar a estimativa com base em uma nova medição.
   float updateEstimate(float mea);
+
+  // Métodos para ajustar os parâmetros do filtro.
   void setMeasurementError(float mea_e);
   void setEstimateError(float est_e);
   void setProcessNoise(float q);
+
+  // Métodos para recuperar valores do filtro.
   float getKalmanGain();
   float getEstimateError();
 
 private:
-  float _err_measure;
-  float _err_estimate;
-  float _q;
-  float _current_estimate = 0;
-  float _last_estimate = 0;
-  float _kalman_gain = 0;
-
+  // Variáveis privadas para armazenar o estado do filtro.
+  float _err_measure;        // Erro de medição.
+  float _err_estimate;       // Erro de estimativa.
+  float _q;                  // Ruído do processo.
+  float _current_estimate;   // Estimativa atual.
+  float _last_estimate;      // Última estimativa.
+  float _kalman_gain;        // Ganho de Kalman.
 };
 
+// Fim da proteção contra inclusão múltipla.
 #endif
+
